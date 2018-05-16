b5c8b2d45c2df494aa451d5e3deb03a447bdd9e8
Charles Jones <c.jones.3.zero@gmail.com>
2018-05-16 06:04:21 -0600

    Updated Idle Object

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,7 +6,10 @@
 let idleThree = {
   //Object Geometries 
   //Spheres
-  geometrySphereOne: new THREE.SphereGeometry(10, 25, 25),
+  geometrySphereOne: new THREE.SphereGeometry(50, 25, 25),
+  setGeometrySphere() {
+    this.geometrySphereOne.position.set(500, 50, 50);
+  },
 
   //Object Materials
   material: new THREE.MeshPhongMaterial({
@@ -29,6 +32,13 @@ let idleThree = {
     this.dirLight.position.set(setLightOneCoords[0], setLightOneCoords[1], setLightOneCoords[2]);
   },
 
+  //Set Object Speed
+  setMeshOneSpeedX : 0,
+  setMeshOneSpeedY : 0,
+
+  //Set Object Position (useful for resets)
+  setMeshOnePositionX : -25,
+
   //Cameras
   camera: new THREE.PerspectiveCamera(
     5,
@@ -37,6 +47,12 @@ let idleThree = {
     5000
   )
 }
+
+/*
+ *
+ * End IDLE OBJECT
+ *
+ */
 
 //Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -62,18 +78,46 @@ requestAnimationFrame(render);
 function render() {
   mesh.rotation.x += Math.sin(-0.05);
   mesh.rotation.y += 0.15;
+  mesh.position.x += idleThree.setMeshOneSpeedX;
+  mesh.position.y += idleThree.setMeshOneSpeedY;
   mesh.rotation.z += Math.PI / 32;
   renderer.render(scene, idleThree.camera);
   requestAnimationFrame(render);
 }
 
-/*
- *
- * END IDLE OBJECT
- *
- */
+//We need this to get the coordinates so we can update functions based on x/y pos
+scene.updateMatrixWorld(true);
+const positionVectorGLOBAL = new THREE.Vector3();
+positionVectorGLOBAL.setFromMatrixPosition( mesh.matrixWorld );
 
 //Grabs the material and tries to change a value
-let changeMaterial = () => {
-  material.color = new THREE.Color(0xff0000)
+let moveIdleThreeRight = () => {
+  idleThree.setMeshOneSpeedX += 1;
 }
+
+//Checks the x pos of idleThree, and if it is higher than the threshold, rest position to NEGATIVE
+//NEGATIVE - Item won't appear so abruptly if it starts slowly from left side
+let checkIdleThreePositionX = () => {
+
+  //Need a fun way to check your x/y position of a mesh? see code below and use
+  //scene.updateMatrixWorld(true);
+  const positionVectorGLOBAL = new THREE.Vector3();
+  positionVectorGLOBAL.setFromMatrixPosition( mesh.matrixWorld );
+
+  
+  if(positionVectorGLOBAL.x > 250){
+    console.log(positionVectorGLOBAL.x);
+    mesh.position.x = -250;
+  }
+  
+}
+
+/*
+*
+* All SetInterval Functions Go Here
+*
+* This will keep the functions separate so I can order and find them a bit easier
+*
+*/
+
+setInterval(checkIdleThreePositionX, 100);
