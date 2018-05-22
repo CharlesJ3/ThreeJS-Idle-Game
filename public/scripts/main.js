7a43c8db6430a17237140f9a09e76c6825d6e09f
Charles Jones <c.jones.3.zero@gmail.com>
2018-05-21 21:35:54 -0600

    Work Submissions
    Need for work tomorrow; testing orbit stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,10 +4,11 @@
  *
  */
 let idleThree = {
+
   //Object Geometries 
   //Spheres
-  geometrySphereOne: new THREE.SphereGeometry(25, 25, 25),
-  geometrySphereOrbitOne: new THREE.SphereGeometry(5, 45, 45),
+  geometrySphereOne: new THREE.SphereGeometry(12, 32, 32),
+  geometrySphereOrbitOne: new THREE.SphereGeometry(3, 45, 45),
 
   //Object Materials
   material: new THREE.MeshPhongMaterial({
@@ -22,32 +23,32 @@ let idleThree = {
     shininess: 2
   }),
 
+  //External Object Count
+  additionalObjectCount: 10,
+
   //Object Light(s)
-  dirLightColorOne: '0x00ff00',
-  dirLight: new THREE.DirectionalLight(this.dirLightColorOne),
-  setLightOneCoords: [1000, 500, 50],
+  dirLightColorOne: '0x0000ff',
+  dirLight: new THREE.PointLight(0xffffff),
+  setLightOneCoords: [125, 250, 1],
 
+  dirLightColorTwo: '0x4286f4',
+  dirLightTwo: new THREE.PointLight(0xffffff),
+  setLightTwoCoords: [-25, 25, 2],
 
-  dirLightColorTwo: '0x00ff00',
-  dirLightTwo: new THREE.DirectionalLight(this.dirLightColorTwo),
-  setLightTwoCoords: [-1000, 500, 50],
+  dirLightColorThree: '0xd84202',
+  dirLightThree: new THREE.PointLight(0xffffff),
+  setLightThreeCoords: [25, -250, -5],
 
-
-  dirLightColorThree: '0x00ff00',
-  dirLightThree: new THREE.DirectionalLight(this.dirLightColorThree),
-  setLightThreeCoords: [1000, -500, 50],
-
-
-  dirLightColorFour: '0x00ff00',
-  dirLightFour: new THREE.DirectionalLight(this.dirLightColorFour),
-  setLightFourCoords: [-1000, -500, 50],
+  dirLightColorFour: '0x17ff02',
+  dirLightFour: new THREE.PointLight(0xffffff),
+  setLightFourCoords: [-25, -25, -5],
 
   //Set all lights here; run this when needing to update the lightning
   setLight() {
-    this.dirLight.position.set(this.setLightOneCoords[0], this.setLightOneCoords[1], this.setLightOneCoords[2]);
-    this.dirLight.position.set(this.setLightTwoCoords[0], this.setLightTwoCoords[1], this.setLightTwoCoords[2]);
-    this.dirLight.position.set(this.setLightThreeCoords[0], this.setLightThreeCoords[1], this.setLightThreeCoords[2]);
-    this.dirLight.position.set(this.setLightFourCoords[0], this.setLightFourCoords[1], this.setLightFourCoords[2]);
+    this.dirLight.position.set(this.setLightOneCoords[0], this.setLightOneCoords[1], this.setLightOneCoords[2]),
+    this.dirLightTwo.position.set(this.setLightTwoCoords[0], this.setLightTwoCoords[1], this.setLightTwoCoords[2]);
+    this.dirLightThree.position.set(this.setLightThreeCoords[0], this.setLightThreeCoords[1], this.setLightThreeCoords[2]);
+    this.dirLightFour.position.set(this.setLightFourCoords[0], this.setLightFourCoords[1], this.setLightFourCoords[2]);
   },
 
   //Set Object Speed
@@ -59,10 +60,10 @@ let idleThree = {
 
   //Cameras
   camera: new THREE.PerspectiveCamera(
-    120,
+    65,
     window.innerWidth / window.innerHeight,
-    .1,
-    1000
+    2,
+    5000
   )
 }
 /*
@@ -84,6 +85,8 @@ const scene = new THREE.Scene();
 
 //Lights
 scene.add(idleThree.dirLight);
+scene.add(idleThree.dirLightTwo);
+scene.add(idleThree.dirLightThree);
 idleThree.setLight();
 
 //Meshes : TODO: CONVERT THESE TO IDLE OBJECT INSTEAD OF GLOBAL!!!!!
@@ -92,7 +95,7 @@ const meshTwo = new THREE.Mesh(idleThree.geometrySphereOrbitOne, idleThree.mater
 
 
 //Quick note on meshes; for this project, set them a bit to the negative 
-mesh.position.set(0, 0, -20);
+mesh.position.set(0, 0, -50);
 // meshTwo.position.set(0, 0, -20);
 renderer.render(scene, idleThree.camera);
 scene.add(mesh);
@@ -100,8 +103,8 @@ requestAnimationFrame(render);
 
 function render() {
   // mesh.rotation.x += Math.sin(-0.05);
-  mesh.rotation.y += 0.15;
   // mesh.rotation.z += Math.PI / 32;
+  mesh.rotation.z += 0.0015;
   mesh.position.x += idleThree.setMeshOneSpeedX;
   mesh.position.y += idleThree.setMeshOneSpeedY;
   renderer.render(scene, idleThree.camera);
@@ -126,11 +129,11 @@ let moveIdleThreeLeft = () => {
   mesh.position.set(
     (Math.cos(date) * orbitRadius), 
     Math.sin(date),
-    (Math.sin(date) - 20)
+    (Math.sin(date) - 50)
   );
 
-  console.log(mesh.position.x);
-  console.log(mesh.position.y);
+  // console.log(mesh.position.x);
+  // console.log(mesh.position.y);
   // idleThree.setMeshOneSpeedX -= .1;
 }
 
@@ -226,3 +229,38 @@ let resetIdleThree = () => {
 // setInterval(checkIdleThreePositionXNegative, 250);
 setInterval(moveIdleThreeLeft, 10);
 console.log(mesh.position.x);
+
+
+function createSpheres() {
+//create empty 3d object — group for future spheres
+spheres = new THREE.Object3D();
+//randomly create 80 spheres by loop
+    for (var i = 0; i < idleThree.additionalObjectCount; i++) {
+        var sphere = new THREE.SphereGeometry(.5 , 32, 32, 0, 6.5, 0, 6.5);
+        var material = new THREE.MeshPhongMaterial({
+            color: Math.random() * 0xff00000 - 0xff00000,
+            flatShading: THREE.FlatShading,
+        })
+
+    //creating the mesh and add primitive and material
+        var particle = new THREE.Mesh(sphere, material);
+        let distance = 1;
+        //randomly set position and scale
+        particle.position.x = Math.random() * distance * 5;
+        particle.position.y = Math.random() * -distance * 5;
+        particle.position.z = Math.random() * distance * 5;
+        particle.rotation.y = Math.random() * 3 * Math.PI;
+        particle.rotation.z = Math.random() * 1 * Math.PI;
+        //add particle to the spheres group
+        spheres.add(particle);
+    }
+
+    //correct spheres position relative to the camera
+    spheres.position.y = 0;
+    spheres.position.x = 0;
+    spheres.position.z = -10;
+    spheres.rotation.y = Math.PI * 100;
+    //add spheres to the scene
+    scene.add(spheres);
+  }
+
